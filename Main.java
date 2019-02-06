e1a8d75fc6de540ea1f64041958dd0ac18925dce
Luke Tseng <lukegtseng@gmail.com>
2019-02-06 10:30:43 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
+		int turn = 1;
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter Board Size: ");
 		int size = s.nextInt(); //get size of board
@@ -12,9 +13,18 @@ public class Main {
 			}
 		}
 		createBoard(board);
+		while(checkWin(board) != true || checkTie(board) != true) { //continues running if the game isn't over
+		String str = "";
+			if(turn%2 == 1) {
+				System.out.println("Player 1, your move: (e.g. 1,3)");
+				str = "X";
+			} else {
+				System.out.println("Player 2, your move: (e.g. 1,3)");
+				str = "O";
+			}
+		}
 		
-		
-		
+		turn++; //change turn
 	}
 
 	public static void createBoard(String[][] board) {
@@ -36,15 +46,15 @@ public class Main {
 	}
 	//btw im going to start doing the custom board size since it'll be to much work to do if I dont start on it
 	//so youll have to do the checkWin and checkTie depending on board size
-	public static boolean checkWin(String[] board) { 
+	public static boolean checkWin(String[][] board) { 
 		int size = board.length;
 	}
 
-	public static boolean checkTie(String[] board) {
+	public static boolean checkTie(String[][] board) {
 		int size = board.length;
 	}
 
-	public static boolean placementErrorCheck(String[] board) {
+	public static boolean placementErrorCheck(String[][] board) {
 		int size = board.length;
 	}
 }
