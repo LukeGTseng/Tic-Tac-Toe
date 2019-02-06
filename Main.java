a32c438c7edb67bd9b7175f8ce6327030f2d3df5
Luke Tseng <lukegtseng@gmail.com>
2019-02-06 08:45:18 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public static void main(String[] args) {
+
+}
+
+public static void createBoard() {
+
+}
+
+public static boolean checkWin() {
+
+}
+
+public static boolean checkTie() {
+
+}
+
+public static boolean placementErrorCheck() {
+
+}
