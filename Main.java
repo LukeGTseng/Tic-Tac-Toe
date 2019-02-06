fa4ce75c071ae679d9fb23c2e73251a9d0040d46
Luke Tseng <lukegtseng@gmail.com>
2019-02-06 09:20:24 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,23 @@
 import java.util.*;
 
-public static void main(String[] args) {
-
-}
-
-public static void createBoard(String[] board) {
-
-}
-
-public static boolean checkWin(String[] board) {
-
-}
-
-public static boolean checkTie(String[] board) {
-
-}
-
-public static boolean placementErrorCheck(String[] board) {
-
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void createBoard(String[] board) {
+		System.out.println();
+	}
+	
+	public static boolean checkWin(String[] board) {
+	
+	}
+	
+	public static boolean checkTie(String[] board) {
+	
+	}
+	
+	public static boolean placementErrorCheck(String[] board) {
+	
+	}
 }
