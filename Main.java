6c24f19d5c1aa68bdcb3649530bae01d626a03b2
Luke Tseng <lukegtseng@gmail.com>
2019-02-07 14:18:14 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
+		String[] validChars = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "," };
 		boolean legalMove = false;
-		int turn = 1;
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter Board Size: ");
 		int size = s.nextInt(); // get size of board
@@ -13,22 +13,38 @@ public class Main {
 				board[i][j] = " ";
 			}
 		}
-		while (checkWin(board) != true || checkTie(board) != true) { // continues running if the game isn't over
+		int turn = 1;
+		while (checkWin(board) != true || checkTie(board, turn) != true) { // continues running if the game isn't over
 			String str = "";
 			createBoard(board);
 			legalMove = false;
-			if (turn % 2 == 1) {
+			if (checkWin(board) == true) {
+				if (turn % 2 == 1) {
+					System.out.println("Player 2 has won the game!");
+				} else if (turn % 2 == 0) {
+					System.out.println("Player 1 has won the game!");
+				}
+				break;
+			} else if (checkTie(board, turn) == true) {
+				System.out.println("The game is a Tie!");
+				break;
+			} else if (turn % 2 == 1) {
+
 				System.out.println("Player 1, your move: (e.g. 1,3)");
 				str = "X";
-			} else if (turn % 2 == 0){
+			} else if (turn % 2 == 0) {
 				System.out.println("Player 2, your move: (e.g. 1,3)");
 				str = "O";
 			}
 			Scanner move = new Scanner(System.in);
 			while (legalMove == false) {
 				String input = move.next();
+				for (int i = 0; i < input.length() - 1; i++) {
+
+				}
+
 				int x = (Integer.parseInt(input.substring(0, input.indexOf(",")))) - 1;
-				int y = (Integer.parseInt(input.substring(input.indexOf(",") +1, input.length()))) - 1;
+				int y = (Integer.parseInt(input.substring(input.indexOf(",") + 1, input.length()))) - 1;
 				if (placementErrorCheck(board, x, y) == true) {
 					board[x][y] = str;
 					legalMove = true;
@@ -39,7 +55,6 @@ public class Main {
 			turn++;
 		}
 
-		turn++; // change turn
 	}
 
 	public static void createBoard(String[][] board) {
@@ -59,29 +74,106 @@ public class Main {
 		}
 		System.out.println("+");
 	}
-
-	// btw I'm going to start doing the custom board size since it'll be to much
-	// work to do if I don't start on it
-	// so you'll have to do the checkWin and checkTie depending on board size
-	public static boolean checkWin(String[][] board) {
+	
+	public static boolean checkWin(String[][] board) { // using all the check methods to checking for win
 		int size = board.length;
+		// rows
+		if (checkRow(board, size) == true) {
+			return true;
+		} else if (checkColumn(board, size) == true) {
+			return true;			
+		} else if (checkDiag(board, size) == true) {
+			return true;
+		} else if (checkOtherDiag(board, size) == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean checkRow(String[][] board, int size) { //checks each row for win
+		String[] list = new String[size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				list[j] = board[i][j];
+			}
+			if (equalCheck(list) == true) {
+				return true;
+			}
+		}
 		return false;
 	}
 
-	public static boolean checkTie(String[][] board) {
-		int size = board.length;
+	public static boolean checkDiag(String[][] board, int size) { // checks up to down diagonal for win
+		String[] list = new String[size];
+		for (int i = 0; i < size; i++) {
+			list[i] = board[i][i];
+		}
+		if (equalCheck(list) == true) {
+			return true;
+		}
 		return false;
 	}
 
-	public static boolean placementErrorCheck(String[][] board, int x, int y) {
+	public static boolean checkOtherDiag(String[][] board, int size) { // checks down to up diagonal for win
+		String[] list = new String[size];
+		int j = size-1;
+		for (int i = 0; i < size; i++) {
+			list[i] = board[i][j];
+			if(j>0) {
+				j--;
+			}
+		}
+		if (equalCheck(list) == true) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean checkColumn(String[][] board, int size) { // checks each column for win
+		String[] list = new String[size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				list[j] = board[j][i];
+			}
+			if (equalCheck(list) == true) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static boolean checkTie(String[][] board, int turn) { // checks tie based on if every space is used using turns
+		int size = board.length;
+		size = size * size;
+		if (size < turn && checkWin(board) == false) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean placementErrorCheck(String[][] board, int x, int y) { // invalid 
 		int size = board.length;
 		if (x >= size || y >= size) {
 			return false;
-		}
-		if (!board[x][y].equals(" ")) {
+		} else if (!board[x][y].equals(" ")) {
 			return false;
 		} else {
 			return true;
 		}
+	}
+
+	public static boolean equalCheck(String[] values) {
+		String checkValue = values[0];
+		if (checkValue.equals(" ")) {
+			return false;
+		}
+		for (int i = 1; i < values.length; i++) {
+			if (!values[i].equals(checkValue)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
