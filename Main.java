14ffcf6adf77561403959d397352c86fb0d4679f
Luke Tseng <lukegtseng@gmail.com>
2019-02-07 14:54:34 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		String[] validChars = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "," };
 		boolean legalMove = false;
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter Board Size: ");
